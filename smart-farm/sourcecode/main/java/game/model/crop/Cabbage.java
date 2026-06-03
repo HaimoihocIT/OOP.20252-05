631972e34c12d00dba78b418c92b13c04fc11e3c
Admin <Duc.LA2417112@sis.hust.edu.vn>
2026-06-03 20:31:44 +0700

    feat: add concrete subclasses for Crop

---
+++ smart-farm/sourcecode/main/java/game/model/crop/Cabbage.java
@@ -0,0 +1,14 @@
+package game.model.crop;
+
+import game.graphics.Sprite;
+
+public class Cabbage extends Crop {
+    public Cabbage() {
+        super(5, 40, 30, 60, 10, 3, 20);
+    }
+
+    @Override
+    public Sprite getSprite() {
+        return null;
+    }
+}
---
+++ smart-farm/sourcecode/main/java/game/model/crop/Corn.java
@@ -0,0 +1,14 @@
+package game.model.crop;
+
+import game.graphics.Sprite;
+
+public class Corn extends Crop {
+    public Corn() {
+        super(6, 80, 40, 70, 20, 3, 20);
+    }
+
+    @Override
+    public Sprite getSprite() {
+        return null;
+    }
+}
