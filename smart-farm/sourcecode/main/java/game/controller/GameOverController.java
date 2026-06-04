fbe3e01c7c0475b5dcf398dabb9c04dae2f12f39
HaimoihocIT <daihai288@gmail.com>
2026-06-04 10:35:44 +0700

    feat: add GameOverController with restart and exit options

---
+++ smart-farm/sourcecode/main/java/game/controller/GameOverController.java
@@ -0,0 +1,25 @@
+package game.controller;
+
+import game.GameContext;
+import game.GameState;
+import game.input.InputManager;
+
+public class GameOverController implements StateUpdater{
+    @Override
+    public void update(GameContext ctx, InputManager inputManager){
+        int mx = ctx.mouse.getX() / ctx.scale;
+        int my = ctx.mouse.getY() / ctx.scale;
+
+        if (inputManager.isJustPressed(ctx.mouse.getButton() == 1, "mouseLeft")){
+            if (mx >= 155 && mx <= 155 + 90) {
+                if (my >= 120 && my <= 120 + 27) {
+                    ctx.reset();
+                    ctx.handler.setState(GameState.GAME);
+                } else if (my >= 155 && my <= 155 + 27) {
+                    ctx.hasActiveGame = false;
+                    ctx.handler.setState(GameState.MENU);
+                }
+            }
+        }
+    }
+}
