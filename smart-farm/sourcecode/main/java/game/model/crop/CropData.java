5f22bf94bec96174e0a3496cfa04e8eefb2cc26a
Admin <Duc.LA2417112@sis.hust.edu.vn>
2026-06-06 23:35:39 +0700

    refactor(model): remove Sprite attribute and update ALL_CROPS list

--- smart-farm/sourcecode/main/java/game/model/crop/CropData.java
+++ smart-farm/sourcecode/main/java/game/model/crop/CropData.java
@@ -1,7 +1,5 @@
 package game.model.crop;
 
-import game.graphics.Sprite;
-
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -14,24 +12,23 @@ public class CropData {
     public final int maxMoistureThreshold;
     public final int dailyNutrientRequirement;
     public final int harvestValue;
-    public final Sprite matureSprite;
 
     private final Supplier<Crop> factory;
 
     private static final List<CropData> ALL_CROPS = List.of(
-        new CropData("Rice", 36, 8, 20, 50, 80, 20, 100, Sprite.sRice4, Rice::new),
-        new CropData("Cabbage", 12, 5, 10, 30, 60, 20, 40, Sprite.sCabbage4, Cabbage::new),
-        new CropData("Corn", 28, 6, 20, 40, 70, 20, 80, Sprite.sCorn4, Corn::new),
-        new CropData("Carrot", 8, 4, 10, 20, 50, 10, 30, Sprite.sCarrot4, Carrot::new),
-        new CropData("Radish", 5, 4, 10, 20, 50, 10, 20, Sprite.sRadish4, Radish::new),
-        new CropData("Tomato", 16, 6, 20, 30, 60, 20, 50, Sprite.sTomato4, Tomato::new),
-        new CropData("Pumpkin", 50, 9, 30, 50, 80, 30, 140, Sprite.sPumpkin4, Pumpkin::new),
-        new CropData("Eggplant", 32, 7, 20, 40, 70, 30, 90, Sprite.sEggplant4, Eggplant::new),
-        new CropData("Chilli", 24, 6, 20, 30, 60, 30, 70, Sprite.sChili4, Chili::new),
-        new CropData("Pepper", 20, 6, 20, 30, 60, 20, 60, Sprite.sPepper4, Pepper::new)
+            new CropData("Rice", 20, 6, 20, 50, 80, 20, 65, Rice::new),
+            new CropData("Cabbage", 12, 5, 10, 30, 60, 20, 40, Cabbage::new),
+            new CropData("Corn", 28, 6, 20, 40, 70, 20, 80, Corn::new),
+            new CropData("Carrot", 8, 4, 10, 20, 50, 10, 30, Carrot::new),
+            new CropData("Radish", 5, 4, 10, 20, 50, 10, 20, Radish::new),
+            new CropData("Tomato", 16, 6, 20, 30, 60, 20, 50, Tomato::new),
+            new CropData("Pumpkin", 50, 9, 30, 50, 80, 30, 140, Pumpkin::new),
+            new CropData("Eggplant", 32, 7, 20, 40, 70, 30, 90, Eggplant::new),
+            new CropData("Chili", 24, 6, 20, 30, 60, 30, 70, Chili::new),
+            new CropData("Pepper", 20, 6, 20, 30, 60, 20, 60, Pepper::new)
     );
 
-    public CropData(String name, int cost, int growthDays, int dailyWaterRequirement, int minMoistureThreshold, int maxMoistureThreshold, int dailyNutrientRequirement, int harvestValue, Sprite matureSprite, Supplier<Crop> factory){
+    public CropData(String name, int cost, int growthDays, int dailyWaterRequirement, int minMoistureThreshold, int maxMoistureThreshold, int dailyNutrientRequirement, int harvestValue, Supplier<Crop> factory){
         this.name = name;
         this.cost = cost;
         this.growthDays = growthDays;
@@ -40,7 +37,6 @@ public class CropData {
         this.maxMoistureThreshold = maxMoistureThreshold;
         this.dailyNutrientRequirement = dailyNutrientRequirement;
         this.harvestValue = harvestValue;
-        this.matureSprite = matureSprite;
         this.factory = factory;
     }
 
